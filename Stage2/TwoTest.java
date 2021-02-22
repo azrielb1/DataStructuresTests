7125cc113e2356bfb49f3ed4bc128a10560fb9e2
Avi Radinsky <unoficialubuntu@LAPTOP-A7FDHLQD.localdomain>
2021-02-22 17:39:32 -0500

    idk

--- Stage2/TwoTest.java
+++ Stage2/TwoTest.java
@@ -7,7 +7,8 @@ import java.net.URISyntaxException;
 import edu.yu.cs.com1320.project.Stack;
 import edu.yu.cs.com1320.project.impl.StackImpl;
 import edu.yu.cs.com1320.project.stage2.*;
-public class TwoTest {
+
+public class TwoTest{
     @Test
     public void oneStackImpl(){
         Stack<Integer> stack = new StackImpl();
@@ -23,6 +24,5 @@ public class TwoTest {
     public void p(String s){
         System.out.println(s);
     }
-    
-    
 }
+// testing the function
