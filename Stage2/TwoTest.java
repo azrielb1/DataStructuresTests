8ea8dd18895114aa736f01e4802152b03f697a7d
Avi Radinsky <unoficialubuntu@LAPTOP-A7FDHLQD.localdomain>
2021-02-23 12:43:12 -0500

    Additions made by Azriel Bachrach

--- Stage2/TwoTest.java
+++ Stage2/TwoTest.java
@@ -11,7 +11,7 @@ import edu.yu.cs.com1320.project.stage2.*;
 public class TwoTest{
     @Test
     public void oneStackImpl(){
-        Stack<Integer> stack = new StackImpl();
+        Stack<Integer> stack = new StackImpl<>();
         for(int i = 0; i < 100; i++){
             stack.push(i);
             
@@ -20,9 +20,45 @@ public class TwoTest{
             assertEquals(stack.peek(), i);
             assertEquals(stack.pop(), i);
         }
+        passMessage();
     }
-    public void p(String s){
-        System.out.println(s);
+//Code below belongs to Azriel Bachrach
+    @Test
+    public void simplePushAndPop() {
+        Stack<String> s = new StackImpl<>();
+        s.push("one");
+        s.push("two");
+        s.push("three");
+        assertEquals(3, s.size());
+        assertEquals("three", s.peek());
+        assertEquals("three", s.pop());
+        assertEquals("two", s.peek());
+        assertEquals("two", s.peek());
+        assertEquals(2, s.size());
+        assertEquals("two", s.pop());
+        assertEquals("one", s.pop());
+        assertEquals(0, s.size());
+        passMessage();
+    }
+
+    @Test
+    public void aLotOfData() {
+        Stack<Integer> s = new StackImpl<>();
+        for (int i = 0; i < 1000; i++) {
+            s.push(i);
+            assertEquals((Integer)i, s.peek());
+        }
+        assertEquals(1000, s.size());
+        assertEquals((Integer)999, s.peek());
+        for (int i = 999; i >= 0; i--) {
+            assertEquals((Integer)i, s.peek());
+            assertEquals((Integer)i, s.pop());
+        }
+        assertEquals(0, s.size());
+        passMessage();
+    }
+    public void passMessage(){
+        System.out.println("\nPassed test \"" + Thread.currentThread().getStackTrace()[2].getMethodName() + "\"\n");
     }
 }
 // testing the function
